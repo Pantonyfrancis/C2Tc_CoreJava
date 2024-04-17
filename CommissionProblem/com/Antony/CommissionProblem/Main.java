f0881d9c573f7cdc42ab49c53bcd7d50f28313d2
admin <admin@DESKTOP-H3TQ59F>
2024-04-17 11:26:53 +0530

    FirstCommit

---
+++ CommissionProblem/com/Antony/CommissionProblem/Main.java
@@ -0,0 +1,14 @@
+package com.Antony.CommissionProblem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student student = new Student();
+		CommissionProblem commission = new CommissionProblem();
+		commission.Accept_Details_of_an_Employee();
+		double commissionAmount = commission.CalculateCommission();
+		System.out.println("The commission Amount is :"+commissionAmount);
+		
+	}
+
+}
